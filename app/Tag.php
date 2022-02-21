763124927dd2d14dbda269692899e7c3872755a2
Marco Cusenza <marco.cusenza.me@gmail.com>
2022-02-21 21:19:40 +0100

    fix tags db

--- app/Tag.php
+++ app/Tag.php
@@ -2,9 +2,9 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Database\Eloquent\Model;
 
-class Tag extends Pivot
+class Tag extends Model
 {
   public function posts()
   {
